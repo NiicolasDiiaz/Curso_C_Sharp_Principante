8e75b62281e19403c27bfdb3f573908dd715f687
NiicolasDiiaz <nicolasfelipediaz@gmail.com>
2021-07-10 17:27:21 -0500

    Added section 11 with menu

---
+++ Seccion_11_Conceptos_Avanzados/Seccion_11_Conceptos_Avanzados/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Seccion_11_Conceptos_Avanzados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numeroMenu = Menu();
+            while (numeroMenu >= 1 && numeroMenu <= 3)
+            {
+                SelectorMenu(ref numeroMenu);
+                numeroMenu = Menu();
+            }
+        }
+
+        public static int Menu()
+        {
+            Console.WriteLine("Ingrese el punto deseado (1-3)");
+            return int.Parse(Console.ReadLine());
+        }
+        public static void SelectorMenu(ref int numeroMenu)
+        {
+            switch (numeroMenu)
+            {
+                case 1:
+                    Punto1();
+                    break;
+
+                case 2:
+                    Punto2();
+                    break;
+
+                case 3:
+                    Punto3();
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        public static void Punto1() { }
+        public static void Punto2() { }
+        public static void Punto3() { }
+    }
+}
