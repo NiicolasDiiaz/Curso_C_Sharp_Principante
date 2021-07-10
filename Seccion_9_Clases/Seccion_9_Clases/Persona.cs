74a28ec50c7fad40b814f5470aa8abf11fd12c16
NiicolasDiiaz <nicolasfelipediaz@gmail.com>
2021-07-10 12:06:56 -0500

    punto 1

---
+++ Seccion_9_Clases/Seccion_9_Clases/Persona.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seccion_9_Clases
+{
+    class Persona
+    {
+        public string nombre { get; set; }
+        public int edad { get; set; }
+
+        public Persona()
+        {
+            nombre = "";
+            edad = 0;
+        }
+
+        public Persona(string name, int age)
+        {
+            nombre = name;
+            edad = age;
+        }
+
+        public void showInfo()
+        {
+            Console.WriteLine($"El nombre es: {nombre} y la edad es: {edad}");
+        }
+
+    }
+
+}
--- Seccion_9_Clases/Seccion_9_Clases/Program.cs
+++ Seccion_9_Clases/Seccion_9_Clases/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Seccion_9_Clases
 {
@@ -6,7 +7,75 @@ namespace Seccion_9_Clases
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            int numeroMenu = Menu();
+            while (numeroMenu >= 1 && numeroMenu <= 6)
+            {
+                SelectorMenu(ref numeroMenu);
+                numeroMenu = Menu();
+            }
         }
+
+        public static int Menu()
+        {
+            Console.WriteLine("Ingrese el punto deseado (1-6)");
+            return int.Parse(Console.ReadLine());
+        }
+        public static void SelectorMenu(ref int numeroMenu)
+        {
+            switch (numeroMenu)
+            {
+                case 1:
+                    Punto1();
+                    break;
+
+                case 2:
+                    Punto2();
+                    break;
+
+                case 3:
+                    Punto3();
+                    break;
+
+                case 4:
+                    Punto4();
+                    break;
+
+                case 5:
+                    Punto5();
+                    break;
+
+                case 6:
+                    Punto6();
+                    break;
+
+                default:
+                    break;
+            }
+        }
+        public static void Punto1() 
+        {
+            List <Persona> peopleList = new List<Persona>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Ingrese nombre de la persona {i + 1}");
+                String name = Console.ReadLine();
+                Console.WriteLine($"Ingrese edad de la persona {i + 1}");
+                int age = int.Parse(Console.ReadLine());
+
+                peopleList.Add(new Persona(name, age));
+            }
+
+            foreach (var item in peopleList)
+            {
+                item.showInfo();
+            }
+        }
+        public static void Punto2() { }
+        public static void Punto3() { }
+        public static void Punto4() { }
+        public static void Punto5() { }
+        public static void Punto6() { }
     }
 }
