f0a65cb7d33fb0b16052e964c84d920a0e35b9e3
NiicolasDiiaz <nicolasfelipediaz@gmail.com>
2021-07-12 15:02:25 -0500

    Section 13 was created

---
+++ Seccion_13_Archivos_y_Streams/Seccion_13_Archivos_y_Streams/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Seccion_13_Archivos_y_Streams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
