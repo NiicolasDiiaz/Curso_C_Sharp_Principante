2020c446883d5e1fe209b4b04711aec7416c86b1
NiicolasDiiaz <nicolasfelipediaz@gmail.com>
2021-07-12 17:29:06 -0500

    full section

---
+++ Seccion_13_Archivos_y_Streams/Seccion_13_Archivos_y_Streams/Persona.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seccion_13_Archivos_y_Streams
+{
+    class Persona
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+        public string location { get; set; }
+
+        public string InsertPerson()
+        {
+            return name + "|" + age.ToString() + "|" + location + ";";
+        }
+    }
+}
--- Seccion_13_Archivos_y_Streams/Seccion_13_Archivos_y_Streams/Program.cs
+++ Seccion_13_Archivos_y_Streams/Seccion_13_Archivos_y_Streams/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 
 namespace Seccion_13_Archivos_y_Streams
 {
@@ -6,7 +8,71 @@ namespace Seccion_13_Archivos_y_Streams
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string fileName = "registro.txt";
+            string total = "";
+
+            do
+            {
+                Persona person = new Persona();
+                Menu(person);
+                total += person.InsertPerson();
+                Console.WriteLine("¿Desea introducir mas personas (S/N)?");
+            } while (Console.ReadLine() != "N");
+
+            EscribirArchivo(fileName, total);
+
+            try
+            {
+                LeerArchivo(fileName);
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Rectifique que sus datos estan bien escritos");
+            }
+            finally
+            {
+                Console.WriteLine();
+                Console.WriteLine("Estos fueron los datos ingresados: " + total);
+            }
+
+        }
+        public static void Menu(Persona person)
+        {
+            Console.WriteLine("Ingrese nombre de la persona");
+            person.name = Console.ReadLine();
+            Console.WriteLine("Ingrese edad de la persona");
+            person.age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese localidad de la persona");
+            person.location = Console.ReadLine();
+        }
+        public static void EscribirArchivo(string fileName, string total)
+        {
+            FileStream fsWrite = new FileStream(fileName, FileMode.Append);
+            fsWrite.Write(ASCIIEncoding.ASCII.GetBytes(total), 0, total.Length);
+            fsWrite.Close();
+        }
+        public static void LeerArchivo(string fileName)
+        {
+            
+            FileStream fsRead = new FileStream(fileName, FileMode.Open);
+            
+            byte[] readByte = new byte[fsRead.Length];
+            fsRead.Read(readByte, 0, readByte.Length);
+
+            string lectura = ASCIIEncoding.ASCII.GetString(readByte);
+            char[] separadores = new char[] { ';', '|' };
+            string[] paraMostrar = lectura.Split(separadores[0]);
+            foreach (var item in paraMostrar)
+            {
+                string[] finalMostrar = item.Split(separadores[1]);
+                foreach (var item2 in finalMostrar)
+                {
+                    Console.Write(item2 + " ");
+                }
+                Console.WriteLine();
+            }
+            fsRead.Close();
         }
     }
 }
