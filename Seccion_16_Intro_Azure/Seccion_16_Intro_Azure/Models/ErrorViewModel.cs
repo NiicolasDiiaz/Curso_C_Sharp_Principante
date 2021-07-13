cb3f50f6f10c92a9aa35abbf5d63e5673b9baea8
NiicolasDiiaz <nicolasfelipediaz@gmail.com>
2021-07-13 16:28:31 -0500

    Section 16: configuration ready

---
+++ Seccion_16_Intro_Azure/Seccion_16_Intro_Azure/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Seccion_16_Intro_Azure.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Seccion_17_EFC/Seccion_17_EFC/Contexto/ContextoDB.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Seccion_17_EFC.Contexto
+{
+    public class ContextoDB:DbContext
+    {
+        public ContextoDB(DbContextOptions<ContextoDB> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
