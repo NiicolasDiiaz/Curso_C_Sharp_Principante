695b373dbddcdb947b91169fb06cb62280b665b4
NiicolasDiiaz <nicolasfelipediaz@gmail.com>
2021-07-19 12:05:47 -0500

    Creation of contextDB, DB, DB connection and models

---
+++ Proyecto_Factura_V2/Proyecto_Factura_V2/Context/DDBBContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Proyecto_Factura_V2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Proyecto_Factura_V2.Context
+{
+    public class DDBBContext : DbContext
+    {
+        public DDBBContext(DbContextOptions<DDBBContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating (ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
---
+++ Proyecto_Factura_V2/Proyecto_Factura_V2/Models/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Proyecto_Factura_V2.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+
+        [Required(ErrorMessage = "Enter the name of the product")]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Enter the price of the product")]
+        public int Price { get; set; }
+
+        [Required(ErrorMessage = "Enter the manufacturer of the product")]
+        public string Manufacturer { get; set; }
+
+        [Required(ErrorMessage = "Enter the date of entry of the product")]
+        [DataType(DataType.Date)]
+        [Display(Name = "Date of entry")]
+        public DateTime DateOfEntry { get; set; }
+    }
+}
