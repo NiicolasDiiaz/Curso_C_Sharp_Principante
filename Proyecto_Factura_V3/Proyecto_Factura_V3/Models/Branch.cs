83580898e291b1e2b659546e64206144acb55e70
NiicolasDiiaz <nicolasfelipediaz@gmail.com>
2021-07-21 12:52:50 -0500

    Creation of Models

--- Proyecto_Factura_V3/Proyecto_Factura_V3/Models/Branch.cs
+++ Proyecto_Factura_V3/Proyecto_Factura_V3/Models/Branch.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,12 @@ namespace Proyecto_Factura_V3.Models
 {
     public class Branch
     {
+        [Key]
+        public int BranchId { get; set; }
+
+        [Required(ErrorMessage = "Enter the name of the branch")]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
     }
 }
--- Proyecto_Factura_V3/Proyecto_Factura_V3/Models/Receipt.cs
+++ Proyecto_Factura_V3/Proyecto_Factura_V3/Models/Receipt.cs
@@ -7,5 +7,12 @@ namespace Proyecto_Factura_V3.Models
 {
     public class Receipt
     {
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; }
+
+        public List<Product> Products { get; set; }
+
+        public int BranchId { get; set; }
+        public Branch Branch { get; set; }
     }
 }
