71063917a2dadbd7ddb4ba1ec3c932f091d06572
NiicolasDiiaz <nicolasfelipediaz@gmail.com>
2021-07-21 13:00:50 -0500

    Creatioon of Entity Config

--- Proyecto_Factura_V3/Proyecto_Factura_V3/Models/Receipt.cs
+++ Proyecto_Factura_V3/Proyecto_Factura_V3/Models/Receipt.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,9 @@ namespace Proyecto_Factura_V3.Models
 {
     public class Receipt
     {
+        [Key]
+        public int ReceiptId { get; set; }
+
         public int CustomerId { get; set; }
         public Customer Customer { get; set; }
 
